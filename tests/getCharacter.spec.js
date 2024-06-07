20f3471ed4c3ddf9e016bae8fb7bece8c320cbba
Matheus Schittini <matheusschittinimc@gmail.com>
2024-06-07 11:43:22 -0300

    .

--- tests/getCharacter.spec.js
+++ tests/getCharacter.spec.js
@@ -7,19 +7,22 @@ describe('9 - Implemente os casos de teste da função `getCharacter`', () => {
       name: 'Arya Stark',
       class: 'Rogue',
       phrases: ['Not today', 'A girl has no name.'],
-    },)
+    },);
     expect(getCharacter('Brienne')).toEqual({
       name: 'Brienne Tarth',
       class: 'Knight',
       phrases: ['Im No Lady, Your Grace.', 'I, Brienne Of Tarth, Sentence You To Die.'],
-    },)
-    // Teste se a função retorna o objeto correto para o parâmetro 'Melissandre'
+    },);
     expect(getCharacter('Melissandre')).toEqual({
       name: 'Melissandre',
       class: 'Necromancer',
       phrases: ['Death By Fire Is The Purest Death.', 'For The Night Is Dark And Full Of Terrors.'],
-    },)
-    // Teste se os parâmetros não são Case Sensitive.
+    },);
+    expect(getCharacter('Arya')).toEqual(getCharacter('arya'));
+    expect(getCharacter('Brienne')).toEqual(getCharacter('brienne'));
+    expect(getCharacter('Melissandre')).toEqual(getCharacter('melissandre'));
     // Teste se ao passar um nome que não está na tabela, a função retorna undefined.
+    expect(getCharacter('matheus')).toBeUndefined();
+    expect(getCharacter('Fernanda')).toBeUndefined();
   });
 });
